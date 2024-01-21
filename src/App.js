dec8085d9f5bac37053abe83a66594fd66071652
Krishna@5476 <krishnamurali576@gmail.com>
2024-01-21 20:21:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("todos")) || []
+  );
+  const [editId, setEditId] = useState(0);
+  console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      const existing = todos.find((item) => {
+        return item.id === editId;
+      });
+
+      const updatedTodo = todos.map((t) =>
+        t.id === existing.id
+          ? (t = { id: t.id, todo })
+          : { id: t.id, todo: t.todo }
+      );
+      setTodos(updatedTodo);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+
+    if (todo.trim() !== "") {
+      const newTodo = { id: Date.now(), todo };
+      setTodos([newTodo, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const delTodo = todos.filter((to) => to.id !== id);
+    console.log(delTodo);
+    setTodos([...delTodo]);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((to) => to.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h3>Add todo</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          ></input>
+          <button type="submit">{editId ? "Edit" : "Add"}</button>
+        </form>
+
+        <ul>
+          {todos.map((item) => {
+            return (
+              <li key={item.id}>
+                <span>{item.todo}</span>
+                <button onClick={() => handleEdit(item.id)}>Edit</button>
+                <button onClick={() => handleDelete(item.id)}>Delete</button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
